16be91b53c75846aa3406b2ad338145c681b6e89
Mark M. Young <MarkMYoung@users.noreply.github.com>
2018-08-08 18:14:47 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports.TimeDuration = require('./src/TimeDuration');
+module.exports.TimeInterval = require('./src/TimeInterval');
+module.exports.TimeIntervalIterator = require('./src/TimeIntervalIterator');
